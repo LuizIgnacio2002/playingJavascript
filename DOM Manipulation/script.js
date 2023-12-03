d6f8b1e94a74db71796c1c87e762bea9594550dd
Luiz2002 <quineche44@gmail.com>
2023-12-03 07:22:12 +0000

    done

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -15,10 +15,35 @@ title.innerHTML = '<p>Hello</p>'
 
 title.style.color = 'red'
 
-const redDiv = document.getElementById('red')
-const yellowDiv = document.getElementById('yellow')
-const greenDiv = document.getElementById('green')
+// const redDiv = document.getElementById('red')
+// const yellowDiv = document.getElementById('yellow')
+// const greenDiv = document.getElementById('green')
 
-redDiv.onclick = () => console.log('red')
-yellowDiv.onclick = () => console.log('yellow')
-greenDiv.onclick = () => console.log('green')
+// redDiv.onclick = () => console.log('red')
+// yellowDiv.onclick = () => console.log('yellow')
+// greenDiv.onclick = () => console.log('green')
+
+
+
+const squares = document.querySelectorAll('.colorSquare')  
+const timesClicked = {'red': 0, 'yellow':0, 'green': 0}
+
+squares.forEach(square => {
+    square.onclick = () => {
+        timesClicked[square.value] += 1
+        square.innerText = timesClicked[square.value]
+    }
+})
+
+
+
+function clearScore() {
+    timesClicked['red'] = 0
+    timesClicked['yellow'] = 0
+    timesClicked['green'] = 0
+    squares.forEach(square => square.innerText = 0)
+}
+
+const clearGameDiv = document.getElementById('clear-game')
+clearGameDiv.onclick = () => clearScore()
+
